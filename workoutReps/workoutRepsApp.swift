30ddb9860d0013b1ad59832fa74a24a20d47c2a2
Jose Maria Vivanco <jm.vivlo@gmail.com>
2024-03-23 11:19:17 -0600

    Initial Commit

---
+++ workoutReps/workoutRepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutRepsApp.swift
+//  workoutReps
+//
+//  Created by Jose Vivanco on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct workoutRepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
